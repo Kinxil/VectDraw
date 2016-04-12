7a18da19cc604a70f2047ecd45183e19e7483dcd
Kinxil <anthony_mayorserra@yahoo.fr>
2016-04-12 16:04:16 +0200

    Added files via upload

---
+++ dessin.h
@@ -0,0 +1,23 @@
+#ifndef DESSIN_H
+#define DESSIN_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "../Shape/Shapes.h"
+
+class Draw {
+private:
+  std::vector<Shape> shapes;
+  void loadBuffer(std::string *buf, int n, std::ifstream &file);
+  struct rgb str2color (std::string c);
+
+public:
+  Draw();
+  ~Draw();
+  void loadFromFile(std::string f);
+  void drawShapes();
+};
+
+#endif // DESSIN_H
